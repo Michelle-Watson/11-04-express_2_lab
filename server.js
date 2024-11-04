3ec21d1178a6f546bcae0ae11aab36ec259e09be
Michelle-Watson <alicerockshard@gmail.com>
2024-11-04 12:48:16 -0500

    Use diff port to use node --watch server.js, remove commented code, make static page more clear

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import express from "express";
 import cors from "cors";
 
 // Import the modular router that you exported from foodRoutes.js
-import router from "./routes/foodRoutes.js"; //Did you mean to import "./routes/foodRoutes.js"?
+import router from "./routes/foodRoutes.js";
 
 // Create an instance of Express
 const app = express();
@@ -13,7 +13,7 @@ app.use(express.static("public"));
 // SLIDE 9: Middleware in Express
 // Create a middleware function in your server.js file that...
 app.use((req, res, next) => {
-  // logs the path that the request is targeting & time req. was received
+  // log path that the request is targeting & time req. was received
   console.log(req.path, new Date());
   // CONSOLE: /foods/soups 2024-11-04T17:31:13.972Z
   next();
@@ -22,28 +22,11 @@ app.use((req, res, next) => {
 // Enable CORS
 app.use(cors());
 
-// app.get("/", function (req, res) {
-//   // path = req = URL at which the handler (a callback function) will be invoked
-//   res.send("Welcome to /");
-// });
-
 // Use app.use() to mount your food router on to the path /foods
 app.use("/foods", router);
 
-const PORT = 8080;
+const PORT = 8085;
 app.listen(PORT, function () {
   console.log("Hello World");
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-// http://localhost:8080/foods/soups
-
-// GET http://localhost:8080/foods/soups
-// Error: connect ECONNREFUSED 127.0.0.1:8080
-// Request Headers
-// User-Agent: PostmanRuntime/7.42.0
-// Accept: */*
-// Postman-Token: 99ce627f-e157-4d79-a311-2b1571e85f00
-// Host: localhost:8080
-// Accept-Encoding: gzip, deflate, br
-// Connection: keep-alive
